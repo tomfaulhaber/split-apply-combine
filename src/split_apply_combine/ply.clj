ddf707836c86ad9117e7263e55aabed33ec57730
Tom Faulhaber <git_com@infolace.com>
2013-06-05 12:17:51 -0700

    Support for expressions in ddply, mostly untested

--- src/split_apply_combine/ply.clj
+++ src/split_apply_combine/ply.clj
@@ -2,55 +2,79 @@
   (:use [incanter.core :only [$data col-names conj-rows dataset]])
   (:require [split-apply-combine.core :as sac]))
 
-(defn- map-get
-  ([m k]
-     (if (keyword? k)
-       (or (get m k) (get m (name k)))
-       (get m k)))
-  ([m k colnames]
-     (cond
-      (keyword? k)
-        (or (get m k) (get m (name k)))
-      (number? k)
-        (get m (nth colnames k))
-      :else
-        (get m k))))
+(defn fast-conj-rows 
+  "A simple version of conj-rows that runs much faster"
+  [& datasets]
+  (when (seq datasets) 
+    (dataset (col-names (first datasets))
+             (mapcat :rows datasets))))
 
-(defn- submap [m ks]
-  (zipmap (if (coll? ks) ks [ks])
-          (map #(map-get m %) (if (coll? ks) ks [ks]))))
+(defn expr-to-fn
+  [expr]
+  (let [row-param (gensym "row-")
+        kw-map (sac/build-keyword-map expr)]
+    `(fn [~row-param]
+       (let [~@(apply concat
+                      (for [[kw sym] kw-map] 
+                        [sym `(get ~row-param ~kw ~kw)]))]
+         ~(sac/convert-keywords expr kw-map)))))
+
+(defn exprs-to-fns 
+  [group-by]
+  (if (coll? group-by)
+    (for [item group-by] 
+      (if (and (coll? item)
+               (coll? (second item))
+               (not (#{'fn 'fn*} (first (second item)))))
+        [(first item) (expr-to-fn (second item))]
+        item))
+    group-by))
 
 (defn split-ds 
-  "WRITE A DOC STRING"
-  [group-by data]
-   (let [cols (col-names data)]
-     (loop [cur (:rows data) row-groups {}]
-       (if (empty? cur)
-         (for [[group rows] row-groups] [group (dataset cols rows)])
-         (recur (next cur)
-                (let [row (first cur)
-                      k (submap row group-by)
-                      a (row-groups k)]
-                  (assoc row-groups k (if a (conj a row) [row]))))))))
+  ""
+  [group-by-fns data]
+  (let [cols (col-names data)
+        group-by-fn (if (= 1 (count group-by-fns))
+                      (first group-by-fns) 
+                      (apply juxt group-by-fns))]
+    (loop [cur (:rows data) row-groups {}]
+      (if (empty? cur)
+        (for [[group rows] row-groups] [group (dataset cols rows)])
+        (recur (next cur)
+               (let [row (first cur)
+                     k (group-by-fn row)
+                     a (row-groups k)]
+                 (assoc row-groups k (if a (conj a row) [row]))))))))
 
 (defn apply-ds [fun grouped-data] 
   (for [[group split-data] grouped-data] 
     [group (fun split-data)]))
 
+
 (defn combine-ds [group-by grouped-data]
   (let [group-by (if (coll? group-by) group-by [group-by])
         group-by-filter (complement (set group-by))] 
-    (reduce conj-rows 
+    (apply fast-conj-rows 
             (for [[group data] grouped-data]
               (let [union-cols (concat group-by (filter group-by-filter (col-names data)))]
                 (dataset union-cols (map #(merge % group) (:rows data))))))))
 
-(defn ddply
+(defn ddply*
 "WRITE A DOC STRING"
   ([group-by fun]
      (ddply group-by fun $data))
   ([group-by fun data]
-     (->> data
-          (split-ds group-by)
-          (apply-ds fun)
-          (combine-ds group-by))))
+     (let [_ (println group-by) 
+           group-by (if (coll? group-by) group-by [group-by]) 
+           group-by (for [item group-by]
+                      (if (coll? item) item [item item]))
+           _ (println group-by)] 
+       (->> data
+           (split-ds (map second group-by))
+           (apply-ds fun)
+           (combine-ds (map first group-by))))))
+
+(defmacro ddply
+  "WRITE A DOC STRING"
+  [group-by fun data]
+  `(ddply* '~(exprs-to-fns group-by) ~fun ~data))
